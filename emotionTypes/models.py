55ecd07955916d5982b03bbf2ea5f23fcf2767b0
Egor <egorgavrilenko6@gmail.com>
2020-05-02 13:43:58 +0300

    added emotion_number

--- emotionTypes/models.py
+++ emotionTypes/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 
+
 class EmotionTypes(models.Model):
     name = models.CharField(verbose_name='Name', max_length=512)
+    emotion_number = models.IntegerField(verbose_name='emotion_number')
